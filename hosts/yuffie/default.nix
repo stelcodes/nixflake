166bad243e07b35e3b4c3f4c7ffaccff1bf8a994
Stel Clementine <dev@stelclementine.com>
2025-04-20 16:45:56 -0700

    Add yuffie pam-parallel module config

--- hosts/yuffie/default.nix
+++ hosts/yuffie/default.nix
@@ -55,40 +55,20 @@
     "d /srv/multimedia 2770 root multimedia -"
   ];
 
-  # Attempt to make pam-parallel work, but I can't figure it out
-  security.pam.services =
-    let
-      settings = builtins.toJSON
-        {
-          mode = "One";
-          modules = { pam_fprintd = "Fingerprint"; login = "Password"; };
-        };
-      serviceCfg = service: {
-        rules.auth = {
-          pam-parallel = {
-            order = config.security.pam.services.swaylock.rules.auth.fprintd.order - 10;
-            control = "sufficient";
-            modulePath = "${pkgs.pam-parallel}/lib/security/pam_parallel.so";
-            args = [
-              settings
-            ];
-          };
-        };
+  security.pam.pam-parallel = {
+    enable = true;
+    applyToModules = [ "swaylock" "ly" ];
+    methods = {
+      fprint = {
+        description = "Fingerprint";
+        rule = "auth sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so";
       };
-    in
-    (lib.flip lib.genAttrs serviceCfg [
-      "swaylock"
-      "ly"
-    ]) // {
-      login.fprintAuth = false;
-      pam_fprintd = {
-        rules.auth.fprintd = {
-          enable = true;
-          control = "sufficient";
-          modulePath = "${config.services.fprintd.package}/lib/security/pam_fprintd.so";
-        };
+      password = {
+        description = "Password";
+        rule = "auth sufficient ${config.security.pam.package}/lib/security/pam_unix.so likeauth nullok try_first_pass";
       };
     };
+  };
 
   system.stateVersion = "24.11";
 
