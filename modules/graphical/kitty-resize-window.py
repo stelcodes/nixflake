f526a195c8f01595dfa571a01c9cf3a29d2e047b
Stel Clementine <dev@stelclementine.com>
2025-05-03 23:49:07 -0700

    Improve algo in kitty-resize-window kitten

--- modules/graphical/kitty-resize-window.py
+++ modules/graphical/kitty-resize-window.py
@@ -1,64 +1,35 @@
 # Requires single argument, one of: "left" "right" "up" "down"
 # Test within kitten shell: kitten resize_window.py left
-from typing import Dict, List, NamedTuple, Tuple
+import sys
+from subprocess import DEVNULL
+from typing import List
 from kittens.tui.handler import kitten_ui
 
-ResizeParams = NamedTuple(
-    "ResizeCmd", [("axis", str), ("increment", str), ("neighbor", str)]
-)
 
-
-def ls_cmd(r: ResizeParams) -> List[str]:
-    # https://sw.kovidgoyal.net/kitty/remote-control/#kitten-ls
-    return [
-        "ls",
-        "--match=neighbor:" + r.neighbor,
-    ]
-
-
-def resize_cmd(r: ResizeParams) -> List[str]:
-    # https://sw.kovidgoyal.net/kitty/remote-control/#kitten-resize-window
-    return [
-        "resize-window",
-        "--axis=" + r.axis,
-        "--increment=" + r.increment,
-        "--match=neighbor:" + r.neighbor,
-    ]
-
-
-directions: Dict[str, Tuple[ResizeParams, ResizeParams]] = {
-    "left": (
-        ResizeParams("horizontal", "1", "right"),
-        ResizeParams("horizontal", "-1", "left"),
-    ),
-    "right": (
-        ResizeParams("horizontal", "-1", "right"),
-        ResizeParams("horizontal", "1", "left"),
-    ),
-    "up": (
-        ResizeParams("vertical", "-1", "top"),
-        ResizeParams("vertical", "1", "bottom"),
-    ),
-    "down": (
-        ResizeParams("vertical", "1", "top"),
-        ResizeParams("vertical", "-1", "bottom"),
-    ),
-}
-
-
-@kitten_ui(allow_remote_control=True)
+@kitten_ui(allow_remote_control=True, remote_control_password=True)
 def main(args: List[str]) -> str:
-    direction = args[1]
-    # Try first set of resize params
-    params = directions[direction][0]
-    # To avoid annoying error message flash, check if neighbor exists first
-    ls_result = main.remote_control(ls_cmd(params), capture_output=True, text=True)
-    if ls_result.stdout.strip() != "":
-        main.remote_control(resize_cmd(params))
-        return ""
-    # If the first set of resize params were invalid, try the next
-    params = directions[direction][1]
-    ls_result = main.remote_control(ls_cmd(params), capture_output=True, text=True)
-    if ls_result.stdout.strip() != "":
-        main.remote_control(resize_cmd(params))
-        return ""
+    direction = args[1] if len(args) > 1 else None
+    if direction not in ["left", "right", "up", "down"]:
+        raise Exception("Requires single argument: left, right, up, or down")
+    direction_to_neighbor = {
+        "left": "left",
+        "right": "right",
+        "up": "top",
+        "down": "bottom",
+    }
+    direction_to_axis = {
+        "left": "horizontal",
+        "right": "horizontal",
+        "up": "vertical",
+        "down": "vertical",
+    }
+    neighbor = direction_to_neighbor[direction]
+    axis = direction_to_axis[direction]
+    # https://sw.kovidgoyal.net/kitty/remote-control/#kitten-ls
+    ls_cmd = ["ls", "--match=neighbor:" + neighbor]
+    ls_result = main.remote_control(ls_cmd, capture_output=True)
+    increment = "-1" if ls_result.stdout.strip() == b"" else "1"
+    # https://sw.kovidgoyal.net/kitty/remote-control/#kitten-resize-window
+    resize_cmd = ["resize-window", "--axis=" + axis, "--increment=" + increment]
+    main.remote_control(resize_cmd, stderr=DEVNULL)
+    return ""
