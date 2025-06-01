ba810cc3c5b45b8afa127a755ac87ef92ab73f9c
Stel Clementine <dev@stelclementine.com>
2025-06-01 13:07:09 -0700

    Change weather channel app icon

--- hosts/yuffie/home.nix
+++ hosts/yuffie/home.nix
@@ -15,7 +15,7 @@
       pkgs.thunderbird
       (pkgs.createBrowserApp { name = "Bandcamp"; url = "https://bandcamp.com"; })
       (pkgs.createBrowserApp { name = "Discord"; url = "https://app.discord.com"; })
-      (pkgs.createBrowserApp { name = "Weather"; url = "https://weatherstar.netbymatt.com/?kiosk=true"; })
+      (pkgs.createBrowserApp { name = "Weather"; icon = "weather"; url = "https://weatherstar.netbymatt.com/?kiosk=true"; })
       pkgs.kdePackages.k3b
       pkgs.calibre
       pkgs.deploy-rs
