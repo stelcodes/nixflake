e000eee433f5dd85f1d06a7cbb197ace666c3180
Stel Clementine <dev@stelclementine.com>
2025-06-08 23:10:52 -0700

    Attempt to improve battery life with tlp settings

--- modules/battery/default.nix
+++ modules/battery/default.nix
@@ -26,8 +26,19 @@
           MEM_SLEEP_ON_BAT = "deep";
           MEM_SLEEP_ON_AC = "deep";
           # Prevent clicks: https://linrunner.de/tlp/faq/audio.html
-          SOUND_POWER_SAVE_ON_AC = 0;
-          SOUND_POWER_SAVE_ON_BAT = 0;
+          SOUND_POWER_SAVE_ON_AC = 300;
+          SOUND_POWER_SAVE_ON_BAT = 300;
+          # Reduce power usage: https://linrunner.de/tlp/support/optimizing.html
+          CPU_ENERGY_PERF_POLICY_ON_AC = "power";
+          CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+          CPU_BOOST_ON_AC = 0;
+          CPU_BOOST_ON_BAT = 0;
+          CPU_HWP_DYN_BOOST_ON_AC = 0;
+          CPU_HWP_DYN_BOOST_ON_BAT = 0;
+          WIFI_PWR_ON_AC = "on";
+          WIFI_PWR_ON_BAT = "on";
+          PLATFORM_PROFILE_ON_AC = "low-power";
+          PLATFORM_PROFILE_ON_BAT = "low-power";
         };
       };
     };
