7cbaa9a5e5735f2a5879cc872066111db010cb2a
Stel Clementine <dev@stelclementine.com>
2025-06-22 22:38:10 -0700

    Add sora public key and rekey secrets

--- secrets/keys.nix
+++ secrets/keys.nix
@@ -4,10 +4,12 @@ rec {
   adminKeys = {
     yuffie = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICMoyiraMgUblF095KJA1l3h1sHTwjLiIOe5GHy36Hk7 stel@yuffie";
     marlene = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIUIrkV61xmxSAGQLMatmK0hzPvp+Iekq74pW/Weep9a";
+    universal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBKCjwHHrbo1Mn0wlsw3xh0g4NjIVkPeykQLyZr8AjU9";
   };
   systemKeys = {
     yuffie = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBDf8xRlLjwAln+oiJJ0xAiKjIsRauL/kqn044L5atIw";
     aerith = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHq8GNrmE18CJia7L0vZdTFEBEk2+XSzGp44wQYvH/TG";
+    sora = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILehD91ViQ62nG/Fsucel1Evy9wzjhW8IJ+AkR6PQqs3";
   };
   allAdminKeys = builtins.attrValues adminKeys;
   allSystemKeys = builtins.attrValues systemKeys;
