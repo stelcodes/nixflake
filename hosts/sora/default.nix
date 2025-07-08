cac7fe236929e797b11fe5f00e91f52d1281b1c6
Stel Clementine <dev@stelclementine.com>
2025-07-08 06:29:27 -0700

    Add some postgresql config to sora and yuffie

--- hosts/sora/default.nix
+++ hosts/sora/default.nix
@@ -1,4 +1,9 @@
-{ ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 {
 
   imports = [
@@ -19,6 +24,50 @@
 
     # boot.kernel.sysctl = { "vm.swappiness" = 10; };
     # zramSwap.enable = true;
+
+    services = {
+      caddy = {
+        enable = true;
+      };
+      postgresql = {
+        # https://nixos.wiki/wiki/PostgreSQL
+        enable = true;
+        # By default, access DB through a "local" Unix socket
+        # "/var/lib/postgresql" (TCP/IP is disabled by default because it's
+        # less performant and less secure).
+        # "root" and "postgres" system users can log in as "postgres" superuser
+        # root$ psql -U postgres
+        identMap = ''
+          # ArbitraryMapName systemUser DBUser
+          superuser_map      root       postgres
+          superuser_map      postgres   postgres
+          superuser_map         readonly
+          # Let other names login as themselves
+          superuser_map      /^(.*)$    \1
+        '';
+        # With "sameuser" Postgres will allow DB user access only to databases
+        # of the same name. E.g. DB user "mydatabase" will get access to
+        # database "mydatabase" and nothing else. The part map=superuser_map is
+        # optional. One exception is the DB user "postgres", which by default
+        # is a superuser/admin with access to everything.
+        authentication = lib.mkOverride 10 ''
+          #type database  DBuser  auth-method optional_ident_map
+          local sameuser  all     peer        map=superuser_map
+        '';
+        ensureUsers = [
+          {
+            name = config.admin.username;
+            ensureDBOwnership = true;
+            login = true;
+          }
+        ];
+        # https://www.postgresql.org/docs/current/sql-createrole.html
+        # https://www.postgresql.org/docs/current/predefined-roles.html
+        initialScript = pkgs.writeText "init-postgresql-script" ''
+          GRANT pg_read_all_data TO ${config.admin.username};
+        '';
+      };
+    };
 
     system.stateVersion = "25.05";
 
--- hosts/yuffie/default.nix
+++ hosts/yuffie/default.nix
@@ -141,6 +141,54 @@
     };
   };
 
+  services.postgresql = {
+    # https://nixos.wiki/wiki/PostgreSQL
+    enable = true;
+    # https://www.postgresql.org/docs/current/auth-pg-hba-conf.html
+    # *Without a "local" record, Unix-domain socket connections are disallowed
+    # *TCP/IP is less performant and less secure
+    # local: matches connection attempts using Unix-domain sockets
+    # sameuser: matches if the requested database has the same name as the requested user
+    # all: matches for all DB users
+    # peer: Obtain unix username from the kernel ("local" only)
+    # map: Use this mapping for unix users -> DB users
+    # https://www.postgresql.org/docs/current/auth-peer.html
+    authentication = lib.mkOverride 10 ''
+      #Type Database  DBUser  Method  Options
+      ###############################################
+      local sameuser  all     peer    map=user_map
+    '';
+    # User Name Maps: https://www.postgresql.org/docs/current/auth-username-maps.html
+    # "___ operating system user is allowed to connect as ___ database user"
+    identMap = ''
+      # MapName     SystemUser    DBUser
+      ###############################################
+      # Let root and postgres log in as superuser
+      user_map      root          postgres
+      user_map      postgres      postgres
+      # Let other users login as themselves
+      user_map      /^(.*)$       \1
+    '';
+    # Create a toy database for the admin user
+    ensureDatabases = [ config.admin.username "test123" ];
+    # https://www.postgresql.org/docs/current/sql-createrole.html
+    ensureUsers = [
+      {
+        # DB for admin experiments
+        name = config.admin.username;
+        ensureDBOwnership = true;
+        ensureClauses = {
+          login = true;
+        };
+      }
+    ];
+    # https://www.postgresql.org/docs/current/predefined-roles.html
+    initialScript = pkgs.writeText "init-postgresql-script" ''
+      GRANT pg_read_all_data TO ${config.admin.username};
+    '';
+    # psql cheatsheet: https://tomcam.github.io/postgres/
+  };
+
   system.stateVersion = "24.11";
 
 }
