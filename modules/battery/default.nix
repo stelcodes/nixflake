f714b35ca08a97b5caec267c72d45ab8cd16edd8
Stel Clementine <dev@stelclementine.com>
2025-07-08 14:43:45 -0700

    Tweak tlp settings

--- modules/battery/default.nix
+++ modules/battery/default.nix
@@ -29,16 +29,25 @@
           SOUND_POWER_SAVE_ON_AC = 300;
           SOUND_POWER_SAVE_ON_BAT = 300;
           # Reduce power usage: https://linrunner.de/tlp/support/optimizing.html
-          CPU_ENERGY_PERF_POLICY_ON_AC = "power";
+          CPU_ENERGY_PERF_POLICY_ON_AC = "default";
           CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-          CPU_BOOST_ON_AC = 0;
+          PLATFORM_PROFILE_ON_AC = "balanced";
+          PLATFORM_PROFILE_ON_BAT = "low-power";
+          CPU_BOOST_ON_AC = 1;
           CPU_BOOST_ON_BAT = 0;
-          CPU_HWP_DYN_BOOST_ON_AC = 0;
+          CPU_HWP_DYN_BOOST_ON_AC = 1;
           CPU_HWP_DYN_BOOST_ON_BAT = 0;
           WIFI_PWR_ON_AC = "on";
           WIFI_PWR_ON_BAT = "on";
-          PLATFORM_PROFILE_ON_AC = "low-power";
-          PLATFORM_PROFILE_ON_BAT = "low-power";
+          # https://linrunner.de/tlp/settings/runtimepm.html#runtime-pm-on-ac-bat
+          RUNTIME_PM_ON_AC = "auto";
+          RUNTIME_PM_ON_BAT = "auto";
+          PCIE_ASPM_ON_AC = "powersave";
+          PCIE_ASPM_ON_BAT = "powersave";
+          # https://linrunner.de/tlp/settings/usb.html
+          USB_AUTOSUSPEND = 1;
+          USB_EXCLUDE_AUDIO = 0;
+          USB_ALLOWLIST = "32ac:0002"; # Framwork USB HDMI Expansion Card
         };
       };
     };
