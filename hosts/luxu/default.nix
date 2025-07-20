bcf60e413012f37860836f7e086b1683b3e6ce85
Stel Clementine <dev@stelclementine.com>
2025-07-20 16:34:08 -0500

    Improve luxu config

--- hosts/luxu/default.nix
+++ hosts/luxu/default.nix
@@ -9,7 +9,7 @@
   # nix shell nixpkgs#qemu
   # qemu-system-x86_64 -enable-kvm -m 8192 -cdrom ./result/iso/*.iso
 
-  import = [
+  imports = [
     # For QEMU testing
     (modulesPath + "/profiles/qemu-guest.nix")
   ];
@@ -26,9 +26,18 @@
     };
 
     boot = {
-      kernelModules = [ "wl" ];
+      kernelModules = [ "wl" "usbhid" ];
       extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
     };
+
+    time.timeZone = "America/Chicago";
+
+    networking.networkmanager = {
+      enable = true;
+      dns = "systemd-resolved";
+    };
+
+    hardware.enableRedistributableFirmware = true;
 
     system.stateVersion = "25.05";
 
