55ae0cb983f55487a60f6fa33dd149c989f7bfac
Stel Clementine <dev@stelclementine.com>
2025-07-20 09:41:26 -0500

    Package default wallpaper for niri

--- hosts/luxu/home.nix
+++ hosts/luxu/home.nix
@@ -1,8 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 {
-  home.file."pictures/wallpaper/2f20c35a7e430a9edcbece0f9c24f280.jpg".source = pkgs.fetchurl {
-    url = "https://i.imgur.com/jo5NfMD.jpeg";
-    hash = "sha256-iO7ZO5wM4bx13uOtGOWEIjN89bi+jiSe0zNWbKHGTyY=";
-  };
   home.stateVersion = "25.05";
 }
--- modules/graphical/graphical-linux.home.nix
+++ modules/graphical/graphical-linux.home.nix
@@ -309,6 +309,11 @@ in
         size = 24;
         gtk.enable = true;
       };
+      # Default wallpaper
+      file."pictures/wallpaper/2f20c35a7e430a9edcbece0f9c24f280.jpg".source = pkgs.fetchurl {
+        url = "https://i.imgur.com/jo5NfMD.jpeg";
+        hash = "sha256-iO7ZO5wM4bx13uOtGOWEIjN89bi+jiSe0zNWbKHGTyY=";
+      };
     };
 
     xdg = {
